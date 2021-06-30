7fdf7319660830ba01fc41424062e5a0a77adb12
Hampus <57360212+h-hellstrom@users.noreply.github.com>
2021-06-30 11:47:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ setup(
     license="MIT",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/h-hellstrom/json-normalize",
+    url="https://github.com/funnel-io/json-normalize",
     author="Hampus Hellström",
     author_email="hampus@funnel.io",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires="~=3.7",
     project_urls={
-        "Bug Reports": "https://github.com/h-hellstrom/json-normalize/issues",
-        "Source": "https://github.com/h-hellstrom/json-normalize",
+        "Bug Reports": "https://github.com/funnel-io/json-normalize/issues",
+        "Source": "https://github.com/funnel-io/json-normalize",
     },
 )

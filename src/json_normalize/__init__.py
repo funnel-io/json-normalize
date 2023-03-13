0d8646ddf455d9675909f1f91f9172629386d1f9
Linus Huzell <linus.huzell@funnel.io>
2023-03-13 11:47:50 +0100

    Add __version__ attribute to init file

--- src/json_normalize/__init__.py
+++ src/json_normalize/__init__.py
@@ -1,9 +1,11 @@
 from .main import json_normalize
 from .key_joiners import last_node_name
 
-VERSION = "1.0.0"
-
 __all__ = [
     json_normalize,
     last_node_name,
 ]
+
+__version__ = "1.0.1"
+
+VERSION = __version__

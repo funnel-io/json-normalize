8bd7cc270c03b98554cc0114cca202dba08401f6
Lennart Fridén <lennart@devl.se>
2024-03-28 08:59:41 +0100

    Fix __all__ to contain strings
    For reference, see https://docs.python.org/3/tutorial/modules.html#importing-from-a-package

--- src/json_normalize/__init__.py
+++ src/json_normalize/__init__.py
@@ -2,8 +2,8 @@ from .main import json_normalize
 from .key_joiners import last_node_name
 
 __all__ = [
-    json_normalize,
-    last_node_name,
+    "json_normalize",
+    "last_node_name",
 ]
 
 __version__ = "1.0.1"
